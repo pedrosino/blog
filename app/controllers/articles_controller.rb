8502bc88a529dbddfd20fedfa4f0799ffcd2bacb
Pedro Santos <pedrosino@gmail.com>
2015-12-23 20:12:36 -0200

    "Edit action, some boostrap and starting comments"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,45 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
     @article = Article.new(article_params)
 
     if @article.save
+      #flash[:success] = "You created an article!"
       redirect_to @article
     else
       render 'new'
     end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      #flash[:success] = "You updated this article!"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+
+    if @article.destroy
+      #flash[:success] = "Delete article!"
+    else
+      #flash[:danger] = "Could not delete article!"
+    end
+
+    redirect_to articles_path
   end
 
   private
